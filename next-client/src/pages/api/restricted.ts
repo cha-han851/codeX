cc60e1eb66a21866d6313f63bd58fd4e0e493a51
cha-han851 <65326647+cha-han851@users.noreply.github.com>
2023-01-05 18:42:15 +0900

    test4

--- next-client/src/pages/api/restricted.ts
+++ next-client/src/pages/api/restricted.ts
@@ -1,18 +1,16 @@
+import { authOptions } from '../api/auth/[...nextauth]'
 import { unstable_getServerSession } from "next-auth/next"
-import { authOptions } from "./auth/[...nextAuth]"
 
 
-// eslint-disable-next-line import/no-anonymous-default-export
-export default async (req: any, res: any) => {
+export async function handler(req:any, res:any) {
   const session = await unstable_getServerSession(req, res, authOptions)
-  if (session) {
-    res.send({
-      content:
-        "This is protected content. You can access this content because you are signed in.",
-    })
-  } else {
-    res.send({
-      error: "You must be signed in to view the protected content on this page.",
-    })
+
+  if (!session) {
+    res.status(401).json({ message: "You must be logged in." });
+    return;
   }
+
+  return res.json({
+    message: 'Success',
+  })
 }
