930bce8b80123ec64a9b2031283cfd46d4de70f9
cha-han851 <65326647+cha-han851@users.noreply.github.com>
2023-01-05 22:08:55 +0900

    名前変更

--- next-client/src/pages/api/restricted.ts
+++ next-client/src/pages/api/restricted.ts
@@ -1,5 +1,5 @@
 import { unstable_getServerSession } from "next-auth/next"
-import { authOptions } from "./auth/[...nextauth]"
+import { authOptions } from "./auth/[...nextAuth]"
 
 
 // eslint-disable-next-line import/no-anonymous-default-export
